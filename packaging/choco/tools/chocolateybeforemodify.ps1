e8fb32de1b48ce94f902768087562266f12e2fa3
javad <ja7ad@live.com>
2026-09-07 12:19:53 +0330

    chore: removed choco package

--- packaging/choco/tools/chocolateybeforemodify.ps1
+++
@@ -1,4 +0,0 @@
-﻿$ErrorActionPreference = 'Stop'
-
-# Terminate running Hydra processes to prevent file locks during upgrade/uninstall
-Get-Process -Name 'hydra', 'hydra-gui' -ErrorAction SilentlyContinue | Stop-Process -Force -ErrorAction SilentlyContinue
